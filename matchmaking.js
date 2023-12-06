2cc7fc7f78b4ab583396638d4766056877b5b2b3
Ty Valencia <tyvalencia@gmail.com>
2023-12-06 15:31:26 -0800

    Matchmaking algorithm that writes to Firebase

--- matchmaking.js
+++ matchmaking.js
@@ -7,63 +7,67 @@ Fall 2023
 
 /* Matchmaking Algorithm ---------------------------------------------- */
 
-/* --- Get JSON data --- */ 
-function fetchJsonData(url) {
-    return fetch(url).then(response => response.json());
-  }
-  
-/* --- Whenever a user updates their preferences --- */
-async function newPreferences() {
-    const allHomes = await fetchJsonData('data/homeownerData.json');
-    const allUserPreferences = await fetchJsonData('data/userPreferences.json');
-    let matchList = [];
-  
-    allUserPreferences.forEach(userPref => {
-      allHomes.forEach(home => {
-        if (matchesPreferences(userPref, home)) {
-          matchList.push({ user: userPref.id, home: home.id });
-        }
-      });
-    });
-  
-    if (matchList.length !== 0) {
-      handleMatches(matchList);
-    }
-}
-  
-  /* --- Whenever a new home is registered --- */
-  async function newHome() {
-    const allUsers = await fetchJsonData('data/userData.json');
-    const allHomes = await fetchJsonData('data/homeownerData.json');
-    let matchList = [];
-  
-    allHomes.forEach(home => {
-      allUsers.forEach(user => {
-        const userPref = getUserPreference(user.id, allUserPreferences);
-        if (matchesPreferences(userPref, home)) {
-          matchList.push({ user: user.id, home: home.id });
-        }
-      });
-    });
-  
-    if (matchList.length !== 0) {
-      handleMatches(matchList);
-    }
-  }
-  
+export async function matchUsersAndHomes(db) {
+  const homesSnapshot = await db.collection('homeownerData').get();
+  const usersSnapshot = await db.collection('userData').get();
+  const userPreferencesSnapshot = await db.collection('userPreferences').get();
 
-function matchesPreferences(userPref, home) {
-    return userPref.city === home.city && userPref.country === home.country 
-    && userPref.bedrooms === home.bedrooms && userPref.bathrooms === home.bathrooms
-    && userPref.priceLow <= home.price && userPref.priceHigh >= home.price;
+  console.log('matchmaking started');
+
+  let matchList = [];
+
+  for (let homeDoc of homesSnapshot.docs) {
+    const homeData = homeDoc.data();
+
+    for (let userDoc of usersSnapshot.docs) {
+      const userData = userDoc.data();
+    
+      const userPreferencesDoc = userPreferencesSnapshot.docs.find(doc => doc.data().email === userData.email);
+      if (!userPreferencesDoc) {
+        continue; // Skip to the next user
+      }
+    
+      const userPreferences = userPreferencesDoc.data();
+      if (matchesPreferences(userPreferences, homeData)) {
+        matchList.push({ user: userDoc.id, home: homeDoc.id });
+      }
+    }
+  }
+
+  if (matchList.length !== 0) {
+    handleMatches(matchList);
+  } else {
+    console.log('No matches found.');
+  }
 }
-  
-function getUserPreference(userId, preferences) {
-    return preferences.find(pref => pref.id === userId);
+
+function matchesPreferences(userPreferences, home) {
+  for (let key in userPreferences) {
+    if (key !== 'email' && key !== 'priceLow' && key !== 'priceHigh' && userPreferences[key] !== "" && String(userPreferences[key]) !== String(home[key])) {
+      return false;
+    }
+  }
+
+  if (Number(home.price) < Number(userPreferences.priceLow) || Number(home.price) > Number(userPreferences.priceHigh)) {
+    return false;
+  }
+
+  return true;
 }
-  
+
 function handleMatches(matchList) {
-    // Implement what you want to do with the matches
-    // For example, send an email, update a database, etc.
+  const db = firebase.firestore(); 
+
+  matchList.forEach(match => {
+    db.collection('matches').add({
+      homeownerID: match.home,
+      userID: match.user
+    })
+    .then((docRef) => {
+      console.log(`Match added with ID: ${docRef.id}`);
+    })
+    .catch((error) => {
+      console.error(`Error adding match: ${error}`);
+    });
+  });
 }
-  
